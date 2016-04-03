fe9ddc604d1ba26d37ad01307cf848989160ad07
miniconfig <dev@miniconfig.com>
2016-04-03 08:19:51 -0400

    Added intents for Go To Bed and Wake Up scripts. Also converted tabs to spaces

--- palafita.py
+++ palafita.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
 #################################################
-#       Palafita				#
+#       Palafita                                #
 #################################################
-#  dev@miniconfig.com   			#
-#						#
+#  dev@miniconfig.com                           #
+#                                               #
 #################################################
 #################################################
 
@@ -23,39 +23,39 @@ def get_entity_type(state):
     return entity_type
 
 def data_init():
-	global MyDataStore
-	MyDataStore = DataStore()
+        global MyDataStore
+        MyDataStore = DataStore()
 
 def data_handler(rawdata):
-	global MyDataStore
-	currentSession = MyDataStore.getSession(rawdata['session'])
-	currentUser = MyDataStore.getUser(rawdata['session'])
-	currentRequest = rawdata['request']
-	response = request_handler(currentSession, currentUser, currentRequest)
-	return json.dumps({"version":appVersion,"response":response},indent=2,sort_keys=True)
+        global MyDataStore
+        currentSession = MyDataStore.getSession(rawdata['session'])
+        currentUser = MyDataStore.getUser(rawdata['session'])
+        currentRequest = rawdata['request']
+        response = request_handler(currentSession, currentUser, currentRequest)
+        return json.dumps({"version":appVersion,"response":response},indent=2,sort_keys=True)
 
 def request_handler(session, user, request):
-	requestType = request['type']
-	
-	if requestType == "LaunchRequest":
-		return launch_request(session, user, request)
-	elif requestType == "IntentRequest":
-		return intent_request(session, user, request)
+        requestType = request['type']
+        
+        if requestType == "LaunchRequest":
+                return launch_request(session, user, request)
+        elif requestType == "IntentRequest":
+                return intent_request(session, user, request)
 
 def launch_request(session, user, request):
-	output_speech = "Welcome to HomeAssistant"
-	output_type = "PlainText"
+        output_speech = "Welcome to HomeAssistant"
+        output_type = "PlainText"
 
-	card_type = "Simple"
-	card_title = "HomeAssistant"
-	card_content = "Hello"
+        card_type = "Simple"
+        card_title = "HomeAssistant"
+        card_content = "Hello"
 
-	response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':False}
+        response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':False}
 
-	return response
+        return response
 
 def intent_request(session, user, request):
-	if request['intent']['name'] == "LocateIntent":
+        if request['intent']['name'] == "LocateIntent":
                 hass_devices = {}
                 user = request['intent']['slots']['User']['value']
                 allStates=remote.get_states(api)
@@ -68,10 +68,10 @@ def intent_request(session, user, request):
                 card_title = "Location"
                 card_content = hass_devices[user]
                 print(output_speech)
-                response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':True}	
+                response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':True}      
                 return response 
 
-	elif request['intent']['name'] == "LockIntent":
+        elif request['intent']['name'] == "LockIntent":
                 matched_lock = False
                 action = request['intent']['slots']['Action']['value']
                 requested_lock = request['intent']['slots']['LockName']['value']
@@ -93,8 +93,28 @@ def intent_request(session, user, request):
                 output_type = "PlainText"
                 response = {"outputSpeech" : {"type":output_type, "text":output_speech}, 'shouldEndSession':True}
                 return response
-                        
-	elif request['intent']['name'] == "CurrentEnergyIntent":
+      
+        elif request['intent']['name'] == "BedIntent":
+                who = request['intent']['slots']['Who']['value']
+                if who == "We are" or who == "We're":
+                   script_name = "script.go_to_bed2"
+                else:
+                   script_name = "script.go_to_bed1"
+                remote.call_service(api, 'script', 'turn_on', {'entity_id': '{}'.format(script_name)}) 
+                output_speech = "Goodnight"
+                output_type = "PlainText"
+                response = {"outputSpeech" : {"type":output_type, "text":output_speech}, 'shouldEndSession':True}
+                return response
+
+        elif request['intent']['name'] == "WakeIntent":
+                script_name = "script.wake_up"
+                remote.call_service(api, 'script', 'turn_on', {'entity_id': '{}'.format(script_name)})
+                output_speech = "Good Morning"
+                output_type = "PlainText"
+                response = {"outputSpeech" : {"type":output_type, "text":output_speech}, 'shouldEndSession':True}
+                return response
+        
+        elif request['intent']['name'] == "CurrentEnergyIntent":
                 energy_usage = remote.get_state(api, 'sensor.energy_usage')
                 output_speech = 'Your {} is {} {}.'.format(energy_usage.attributes['friendly_name'], energy_usage.state, energy_usage.attributes['unit_of_measurement'])
                 output_type = "PlainText"
@@ -105,7 +125,7 @@ def intent_request(session, user, request):
                 response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':False}
                 return response
 
-	elif request['intent']['name'] == "MonthlyEnergyIntent":
+        elif request['intent']['name'] == "MonthlyEnergyIntent":
                 energy_cost = remote.get_state(api, 'sensor.energy_cost')
                 output_speech = 'Your {} is ${}'.format(energy_cost.attributes['friendly_name'], energy_cost.state)
                 output_type = "PlainText"
@@ -116,56 +136,56 @@ def intent_request(session, user, request):
                 response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':False}
                 return response
                 
-	elif request['intent']['name'] ==  "HelpIntent":
-		output_speech = "This is the HomeAssistant app. Right now, you can only ask where someone is, or ask about your energy usage.  But I have big plans. "
-		output_type = "PlainText"
-		card_type = "Simple"
-		card_title = "HelloWorld - Title"
-		card_content = "HelloWorld - This is the Hello World help! Just say Hi"
+        elif request['intent']['name'] ==  "HelpIntent":
+                output_speech = "This is the HomeAssistant app. Right now, you can only ask where someone is, or ask about your energy usage.  But I have big plans. "
+                output_type = "PlainText"
+                card_type = "Simple"
+                card_title = "HelloWorld - Title"
+                card_content = "HelloWorld - This is the Hello World help! Just say Hi"
 
-		response = {"outputSpeech": {"type":output_type,"text":output_speech},'shouldEndSession':False}
+                response = {"outputSpeech": {"type":output_type,"text":output_speech},'shouldEndSession':False}
 
-		return response
-	
-		
-	else:
-		return launch_request(session, user, request) ##Just do the same thing as launch request
+                return response
+        
+                
+        else:
+                return launch_request(session, user, request) ##Just do the same thing as launch request
 
 
 
 
 
 class Session:
-	def __init__(self,sessionData):
-		self.sessionId = sessionData['sessionId']
+        def __init__(self,sessionData):
+                self.sessionId = sessionData['sessionId']
 
 
-	def getSessionID(self):
-		return self.sessionId
+        def getSessionID(self):
+                return self.sessionId
 
 class User:
-	def __init__(self,userId):
-		self.userId = userId
-		self.settings = {}
+        def __init__(self,userId):
+                self.userId = userId
+                self.settings = {}
 
-	def getUserId(self):
-		return self.userId
+        def getUserId(self):
+                return self.userId
 
 class DataStore:
-	def __init__(self):
-		self.sessions = {}
-		self.users = {}
+        def __init__(self):
+                self.sessions = {}
+                self.users = {}
 
-	def getSession(self,session):
-		if session['new'] is True or session['sessionId'] not in self.sessions:
-			self.sessions[session['sessionId']] = Session(session)
+        def getSession(self,session):
+                if session['new'] is True or session['sessionId'] not in self.sessions:
+                        self.sessions[session['sessionId']] = Session(session)
 
-		return self.sessions[session['sessionId']]
+                return self.sessions[session['sessionId']]
 
-	def getUser(self,session):
-		userId = session['user']['userId']
-		if userId not in self.users:
-			self.users[userId] = User(userId)
+        def getUser(self,session):
+                userId = session['user']['userId']
+                if userId not in self.users:
+                        self.users[userId] = User(userId)
 
-		return self.users[userId]
+                return self.users[userId]
 
